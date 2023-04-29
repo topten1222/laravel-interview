2d28742cc661146288e1aad114407ad63232f1b4
montree phromthong <montree.p@digitsoul.com>
2023-04-29 22:27:43 +0700

    initial commit

---
+++ www/app/Http/Controllers/ApiAuthController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+use Laravel\Sanctum\PersonalAccessToken;
+
+class ApiAuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        // Validate request data
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|min:10',
+            'group' => [
+                'required',
+                Rule::in(['Admin', 'Viewer', 'Editor']),
+            ],
+        ]);
+        // Return errors if validation error occur.
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return response()->json([
+                'error' => $errors
+            ], 400);
+        }
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'group' => $request->group,
+        ]);
+        $token = $user->createToken('auth_token', [$request->group])->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        // Return errors if validation error occur.
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return response()->json([
+                'error' => $errors
+            ], 400);
+        }
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+        $user = User::where('email', $request['email'])->firstOrFail();
+        $user->tokens()->delete();
+        $token = $user->createToken('auth_token', [$user->group])->plainTextToken;
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
+
+    public function me(Request $request)
+    {
+        if (!$request->user()->tokenCan('Editor')) {
+            return response()->json([
+                'message' => 'permission denied'
+            ], 403);
+        }
+        return $request->user();
+    }
+}
---
+++ www/routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('login', function(){
+    abort('401');
+})->name('login');
+
+Route::post('/register', 'ApiAuthController@register');
+
+Route::post('/login', 'ApiAuthController@login');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', 'ApiAuthController@me');
+});
